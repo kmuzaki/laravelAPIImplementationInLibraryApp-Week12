831095e1512ca6e94caf27da4cf3b76076ca0b59
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-11-18 13:35:19 +0800

    CRUDMethodsForMember

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class MemberController extends Controller
 {
@@ -11,7 +13,8 @@ class MemberController extends Controller
      */
     public function index()
     {
-        //
+        // Status 200 OK (default)
+        return response()->json(Member::all());
     }
 
     /**
@@ -19,30 +22,58 @@ class MemberController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'student_id' => 'required|unique:members|max:20',
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:members|max:255'
+        ]);
+
+        $member = Member::create($validated);
+        // RESTful status code 201 created
+        return response()->json(['message' => 'Member added successfully', 'data' => $member], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Member $member)
     {
-        //
+        // Status 200 OK (default)
+        return response()->json($member);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Member $member)
     {
-        //
+        $validated = $request->validate([
+            'student_id' => [
+                'sometimes',
+                'max:20',
+                Rule::unique('members')->ignore($member->id)
+            ],
+            'name' => 'sometimes|max:255',
+            'email' => [
+                'sometimes',
+                'email',
+                'max:255',
+                Rule::unique('members')->ignore($member->id)
+            ]
+        ]);
+
+        $member->update($validated);
+        // Status 200 OK (default)
+        return response()->json(['message' => 'Member updated successfully', 'data' => $member]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Member $member)
     {
-        //
+        $member->delete();
+        // RESTful status code 204 No Content
+        return response()->json(null, 204);
     }
 }
