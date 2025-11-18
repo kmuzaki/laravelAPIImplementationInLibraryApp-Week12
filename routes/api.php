0b3bea52be3cfd2dd67887d178eb9ff977bb8cbe
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-11-18 11:08:07 +0800

    createControllersAndAddAPIRoutes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('books', BookController::class);
+Route::apiResource('members', MemberController::class);
